7c9d00ce97cfb557cf514970ad66f1f4c10ecbb5
Tim Rohrer <tim@itstechnical.net>
2020-06-23 11:26:12 -0500

    v0.1 added fixtures

---
+++ src/tests/fixtures/directionsRequest.ts
@@ -0,0 +1,6 @@
+import { FetchRoutesParams } from '../../FetchGoogleRoutes'
+
+export default <FetchRoutesParams>{
+  orderedStops: ['ChIJK-0sC0Fl1oYRFccWTTgtw3M', 'ChIJgdL4flSKrYcRnTpP0XQSojM'],
+  alternativeRoutes: false
+};
---
+++ src/tests/fixtures/fetchGoogRoutesResults.ts
@@ -0,0 +1,17 @@
+
+export default { geocoded_waypoints:
+  [ { geocoder_status: 'OK',
+      place_id: 'ChIJK-0sC0Fl1oYRFccWTTgtw3M',
+      types: [Array] },
+    { geocoder_status: 'OK',
+      place_id: 'ChIJgdL4flSKrYcRnTpP0XQSojM',
+      types: [Array] } ],
+ routes:
+  [ { bounds: [Object],
+      copyrights: 'Map data ©2020 Google, INEGI',
+      legs: [Array],
+      overview_polyline: [Object],
+      summary: 'I-10 E and I-40 E',
+      warnings: [],
+      waypoint_order: [] } ],
+ status: 'OK' }
